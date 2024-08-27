591052b92f5dfd16d86014ce5fefc3f62ba37461
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-08-27 11:55:32 -0400

    P

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector(".sidebar");
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
 
-toggleBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('show-sidebar');
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+
+modalBtn.addEventListener('click', () => {
+  modal.classList.add('open-modal');
 })
 
 closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('show-sidebar');
+  modal.classList.remove('open-modal');
 })
 
 
