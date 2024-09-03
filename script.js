8b70d14556739826afe49b33926dc1ac3ded9c23
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-02 22:30:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
-let a = 'red';
-let b = 'blue';
-j
-let c =  a
-a = b;
-b = c;
+const checkSpeed = (speed) => {
+  const speedLimit = 70;
+  const mPerPoint = 5;
 
-console.log(a);
-console.log(b);
+  if (speed <= speedLimit) {
+    console.log('Ok');
+  } else {
+    const points = Math.floor((speed - speedLimit) / mPerPoint);
+    
+    if(points >= 12) {
+      console.log('License suspended');
+    } else {
+      console.log('Points', points);
+    }
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+checkSpeed(130);
