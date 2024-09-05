f3602025f94a109e7571219789ab6c1bd910d2b1
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-05 10:41:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const max = (a, b) => {
-  return (a > b) ? a : b;
+const fizzBuzz = (input) => {
+
+  if (typeof input !== 'number')
+    return NaN;
+
+  if (input % 15 === 0)
+    return 'FizzBuzz'
+
+  if (input % 3 === 0)
+    return 'Fizz';
+
+  if (input % 5 === 0)
+    return 'Buzz';
+
+  if (input % 3 === 0 && input % 5 === 0)
+    return 'FizzBuzz';
+  
+    return input;
 }
 
-console.log(max(1116, 156));
+console.log(fizzBuzz(false));
