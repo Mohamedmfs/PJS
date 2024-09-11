5dedcc6ef812e7bbf079233b8141c956d82f82ac
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-11 11:24:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const address = {
-  street: '1999 Mlk Jr. Way S',
-  city: 'Columbus',
-  zipcode: 43231
+function Address(street, city, zipcode) {
+  this.street = street,
+  this.city = city,
+  this.zipcode = zipcode
 }
 
-const showAddress = (address) => {
-  for (key in address) 
-    console.log(key, address[key] )
+const newAddress1 = new Address('1999 Mlk Jr. Way S', 'Columbus', 43231);
+const newAddress2 = new Address('1999 Mlk Jr. Way S', 'Columbus', 43231);
+
+const areEqual = (newAddress1, newAddress2) => {
+  return newAddress1.street === newAddress2.street && newAddress1.city === newAddress2.city && newAddress1.zipcode === newAddress2.zipcode
 }
 
-console.log(showAddress(address));
+const areSame = (newAddress1, newAddress2) => {
+  return newAddress1 === newAddress2;
+}
+console.log(areEqual(newAddress1, newAddress2));
+console.log(areSame(newAddress1, newAddress2));
+
