4ff4cf7887a028b69b6ace97cc93d6280e9234ad
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-12 11:23:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-const post = {
-  title: 'Post',
-  body: 'knsjdnsdnlknsdlkn k skfv kjvkjvnvkv ivk vurhr',
-  author: 'Hassan Habib',
-  views: 21,
-  comments: [
-    {author: 'Habib Ali',
-    body: 'dgfbjdnjkbd uisdbjkvnjsv svbjksdhui hviuvbdbsjbs'},
-
-    {author: 'khabib Ali',
-      body: 'dgfbjdnjkbd uisdbjkvnjsv svbjksdhui hviuvbdbsjbs'},
-  ],
-  isLive: true
-}
-
-console.log(post)
-
-function Post(title, body, author) {
-  this.title = title;
-  this.body = body;
-  this.author = author;
-}
-
-const newPost = new Post('my title', 'a', 'khabib')
-
-console.log(newPost);
+const priceRange = [
+  {label: '$', tooltip: 'Inexpensive'},
+  {moderate: '$$'},
+  {expensive: '$$$'}
+]
