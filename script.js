54621f0b28404d26babe8566936000c31f62e5aa
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-13 09:31:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const courses = [
-  {id: 1, name: 'a'},
-  {id: 2, name: 'b'}
+  {id: 1, name: 'c'},
+  {id: 2, name: 'b'},
+  {id: 3, name: 'a'}
 ]
 
 const course = courses.find((course) => {
-  return course.id === 1;
+  return course.id === 3;
 })
 
 console.log(course);
