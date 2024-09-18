5f07fc6264807be483c8fca7e93591221219386a
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-18 10:06:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const numbers = [1, 2, 3, 4];
 
-const includes = () => {
-  
+const getMax = (array) => {
+  if (array.length === 0) return undefined;
+
+  return array.reduce((a, b) => (a > b) ? a : b);
 }
+
+console.log(getMax([]));
