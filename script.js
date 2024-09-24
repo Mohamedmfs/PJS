9e064d7e6e0c0039da36734b7d27744633dfd3cf
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-24 11:03:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,15 @@
 //   }
 // }
 
-// const video = {
-//   title: 'a',
-//   tags: ['a', 'b', 'c'],
-//   showTags() {
-//     const self = this;
-//     self.tags.forEach(function(tag) {
-//       console.log(self.title, tag);
-//     })
-//   }
-// }
-
-// video.showTags();
-
-function playVideo() {
-  console.log(this);
+const video = {
+  title: 'a',
+  tags: ['a', 'b', 'c'],
+  showTags() {
+    this.tags.forEach((tag) => {
+      console.log(this.title, tag);
+      
+    })
+  }
 }
 
-playVideo.call( { name: 'Mosh' });
-playVideo.apply( { name: 'Moshika' });
+video.showTags();
