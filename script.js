d09610fc0daa636b17af580e57838653030d1397
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-25 10:52:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-// const video = {
-//   title: 'a',
-//   play() {
-//     console.log(this);
-//   }
-// }
+const person = {
+  firstName: 'Mosh',
+  lastName: 'Hamedani',
+  get fullName () {
+    return `${person.firstName} ${person.lastName}`
+  },
 
-const video = {
-  title: 'a',
-  tags: ['a', 'b', 'c'],
-  showTags() {
-    this.tags.forEach((tag) => {
-      console.log(this.title, tag);
-      
-    })
+  set fullName(value) {
+    if (typeof value !== 'string')
+      throw new Error('Value is not a string.');
+
+    const parts = value.split(' ');
+    if (parts.length !== 2)
+      throw new Error('Enter a first and last name')
+
+    this.firstName = parts[0];
+    this.lastName = parts[1];
   }
+};
+
+try {
+  person.fullName = null;
 }
 
-video.showTags();
+catch (e) {
+  alert(e)
+}
+console.log(person);
