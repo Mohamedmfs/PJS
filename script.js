5b5bbccac87498f4a6467abfa9970a3b7dc876b0
Mohamedmfs <129542529+Mohamedmfs@users.noreply.github.com>
2024-09-28 11:15:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 // Factory Functions
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log("draw");
-    },
-  };
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     draw: function () {
+//       console.log("draw");
+//     },
+//   };
+// }
+
+// const circle = createCircle(2);
+// console.log(circle);
+
+// Constructor Functions
+function Circle(radius) {
+  this.radius = radius,
+  this.draw = function() {
+    console.log('draw 1')
+  }
 }
 
-const circle = createCircle(2);
-console.log(circle);
+const circle = new Circle(1);
+console.log(circle.draw());
